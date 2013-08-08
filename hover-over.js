57e702a78353bbfd485a33ba6f6f050da4bef994
Natalie Kane <nataliedenisekane@gmail.com>
2013-08-08 19:21:47 +0100

    New Site Developed

---
+++ hover-over.js
@@ -0,0 +1,15 @@
+$('ul.gallery').mouseenter(function(){
+    var image= $(this).find('img'),
+        caption = $(this).find('div');
+
+    caption.width(image.width());
+    caption.height(image.height());
+    caption.fadeIn();
+}).mouseleave(function(){
+     var image= $(this).find('img'),
+        caption = $(this).find('div');
+
+    caption.width(image.width());
+    caption.height(image.height());
+    caption.fadeOut();
+}); 
